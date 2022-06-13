62a77294fff4ada055c3886e11886cf2fdf99cbd
Andrii Telenko <mangolik931@gmail.com>
2022-06-14 00:49:35 +0300

    svg transform lesson

---
+++ 27.MISC_UI_CSS/map.js
@@ -0,0 +1,185 @@
+const dictionary = {
+    Crimea: {
+        population: '1500000',
+        center: 'Simferopol',
+        square: '27000',
+        name: 'Autonomous Republic of Crimea'
+    },
+    Mykolayiv: {
+        population: '1141324',
+        center: 'Mykolaiv',
+        square: '24598',
+        name: "Mykolaiv Oblast"
+    },
+    Chernihiv: {
+        population: '991294',
+        center: 'Chernihiv',
+        square: '31903',
+        name: 'Chernihiv Oblast'
+    },
+    Rivne: {
+        population: '1153000',
+        center: 'Rivne',
+        square: '20051',
+        name: 'Rivne Oblast'
+    },
+    Chernivtsi: {
+        population: '901632',
+        center: 'Chernivtsi',
+        square: '8100',
+        name: 'Chernivtsi Oblast'
+    },
+    "Ivano-Frankivs'k": {
+        population: '1368000',
+        center: "Ivano-Frankivs'k",
+        square: '13928',
+        name: "Ivano-Frankivs'k Oblast"
+    },
+    "Khmel'nyts'kyy": {
+        population: '1264000',
+        center: "Khmel'nyts'kyy",
+        square: '20645',
+        name: "Khmel'nyts'kyy Oblast"
+    },
+    "L'viv": {
+        population: '2512000',
+        center: 'Lviv',
+        square: '21831',
+        name: "Lviv Oblast"
+    },
+    Ternopil: {
+        population: '1039000',
+        center: 'Ternopil',
+        square: '13823',
+        name: 'Ternopil Oblast'
+    },
+    Transcarpathia: {
+        population: '1258155',
+        center: 'Uzhgorod',
+        square: '12753',
+        name: "Zakarpattya Oblast"
+    },
+    Volyn: {
+        population: '1031000',
+        center: 'Lutsk',
+        square: '20143',
+        name: "Volyn Oblast"
+    },
+    Cherkasy: {
+        population: '1220363',
+        center: 'Cherkasy',
+        square: '20916',
+        name: "Cherkasy Oblast"
+    },
+    Kirovohrad: {
+        population: '933109',
+        center: 'Kropyvnytskyi',
+        square: '24588',
+        name: "Kirovograd Oblast"
+    },
+    Kiev: {
+        population: '1781000',
+        center: 'Kyiv',
+        square: '28121',
+        name: "Kyiv Oblast"
+    },
+    Odessa: {
+        population: '2351392',
+        center: 'Odessa',
+        square: '33310',
+        name: "Odessa Oblast"
+    },
+     Vinnytsya: {
+        population: '1575808',
+        center: 'Vinnytsya',
+        square: '26513',
+        name: "Vinnytsya Oblast"
+    },
+    Zhytomyr: {
+        population: '1208000',
+        center: 'Zhytomyr',
+        square: '29827',
+        name: 'Zhytomyr'
+    },
+    Sumy: {
+        population: '1068000',
+        center: 'Sumy',
+        square: '23832',
+        name: 'Sumy Oblast'
+    },
+    "Dnipropetrovs'k": {
+        population: '3177000',
+        center: 'Dnipro',
+        square: '31923',
+        name: "Dnipropetrovs'k Obkast"
+    },
+    "Donets'k": {
+        population: '4123000',
+        center: "Donets'k",
+        square: '26517',
+        name: "Donets'k Oblast"
+    },
+    "Luhans'k": {
+        population: '2167802',
+        center: "Luhans'k",
+        square: '26684',
+        name: "Luhans'k Oblast"
+    },
+    Poltava: {
+        population: '1387000',
+        center: 'Poltava',
+        square: '28750',
+        name: 'Poltava Oblast'
+    },
+    Zaporizhzhya: {
+        population: '1687000',
+        center: 'Zaporizhzhya',
+        square: '27183',
+        name: 'Zaporizhzhya Oblast'
+    },
+    "Kiev City": {
+        population: '2954300',
+        center: 'Kyiv',
+        square: '835',
+        name: "Kyiv"
+    },
+    Kherson: {
+        population: '1028000',
+        center: 'Kherson',
+        square: '28461',
+        name: 'Kherson Oblast'
+    },
+    Sevastopol: {
+        population: '340735',
+        center: 'Sevastopol',
+        square: '864',
+        name: "Sevastopol"
+    },
+};
+
+
+const mapSvg = document.querySelector('svg#ukraine-map');
+
+mapSvg.addEventListener('click', (event) => {
+    const triggerEle = event.target;
+    if (triggerEle.tagName !== 'path') {
+        return;
+    }
+    const regionName = triggerEle.getAttribute("name");
+   
+    alert(JSON.stringify(dictionary[regionName]));
+});
+
+let scale = 1;
+
+const zoomInBtn = document.querySelector('#navigation-zoom-in')
+const zoomOutBtn = document.querySelector('#navigation-zoom-out')
+
+zoomInBtn.addEventListener('click', () => {
+    scale = scale + 0.2;
+    mapSvg.style.transform = `scale(${scale})`;
+})
+zoomOutBtn.addEventListener('click', () => {
+    scale = scale - 0.2;
+    mapSvg.style.transform = `scale(${scale})`;
+})
